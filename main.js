a9bda0df5a73fd8dd4b244784cf5bebd4ee22626
h-dheeptha <73650638+h-dheeptha@users.noreply.github.com>
2020-12-16 11:22:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+function show() {
+  var x = document.getElementById("campus");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function show2() {
+  var x = document.getElementById("achievment");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function show3() {
+  var x = document.getElementById("new");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function show4() {
+  var x = document.getElementById("alumni");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function show0() {
+  var x = document.getElementById("home");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
